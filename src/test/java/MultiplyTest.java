a52058167f36b92f32c162c6d350f2f8e61d1ba9
Igor Zasypkin <zas.i.e@yandex.ru>
2022-12-08 00:32:38 +0300

    multiply completed

---
+++ src/test/java/MultiplyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplyTest { //сделать автотест на актуальность
+           @Test
+        void testSomething() {
+          //  assertEquals ("expected", "actual");
+        }
+    }
