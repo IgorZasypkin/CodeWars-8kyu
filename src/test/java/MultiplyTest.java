32f893ff9671c5391df3877c7e5fbb5683c800c3
Igor Zasypkin <zas.i.e@yandex.ru>
2022-12-08 01:56:57 +0300

    ref

--- src/test/java/MultiplyTest.java
+++ src/test/java/MultiplyTest.java
@@ -3,9 +3,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MultiplyTest { //сделать автотест на актуальность
-           @Test
-        void testSomething() {
-          //  assertEquals ("expected", "actual");
-        }
-    }
+//public class MultiplyTest { //сделать автотест на актуальность
+//           @Test
+//        void testSomething() {
+//          //  assertEquals ("expected", "actual");
+//        }
+//    }
